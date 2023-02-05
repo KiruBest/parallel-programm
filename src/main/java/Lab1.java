e7fd263df4e65aa0351bfdc0791cc84075d5cc0d
k.tsutsurin <k.tsutsurin@dev.userstory.ru>
2023-02-05 14:10:22 +0700

    Lab1 done

--- src/main/java/Lab1.java
+++ src/main/java/Lab1.java
@@ -13,18 +13,22 @@ public class Lab1 {
             File imgFolder = new File(IMG_PATH);
             File[] listOfFiles = Objects.requireNonNull(imgFolder.listFiles());
 
-            for (File file : listOfFiles) {
-                if (file.isFile()) {
-                    String fileName = file.getName();
-                    System.out.println("Обработка файла: " + fileName + "...");
+            for (int i = 0; i < 3; i++) {
+                System.out.println("#" + i + "\n");
 
-                    long time = System.currentTimeMillis();
-                    Color[][] colorArray = ImageUtils.extractBytes(IMG_PATH + fileName);
-                    ImageUtils.invertedImage(colorArray, IMG_PATH + INVERTED_IMG + fileName);
-                    ImageUtils.transformImage(colorArray, ImageUtils.contrastMatrix,
-                            IMG_PATH + CONTRAST_IMG + fileName);
+                for (File file : listOfFiles) {
+                    if (file.isFile()) {
+                        String fileName = file.getName();
+                        System.out.println("Обработка файла: " + fileName + "...");
 
-                    System.out.println("Время обработки = " + (System.currentTimeMillis() - time) + "\n");
+                        long time = System.currentTimeMillis();
+                        Color[][] colorArray = ImageUtils.extractBytes(IMG_PATH + fileName);
+                        ImageUtils.invertedImage(colorArray, IMG_PATH + INVERTED_IMG + fileName);
+                        ImageUtils.transformImage(colorArray, ImageUtils.contrastMatrix,
+                                IMG_PATH + CONTRAST_IMG + fileName);
+
+                        System.out.println("Время обработки = " + (System.currentTimeMillis() - time) + "\n");
+                    }
                 }
             }
         } catch (IOException e) {
